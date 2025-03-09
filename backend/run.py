79a69bb82ad8ec25031319666834931a257fbeb8
Azhar Munir <azharsiddiqui8540@gmail.com>
2025-03-10 04:28:54 +0500

    Added run.py file

--- backend/run.py
+++ backend/run.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Append the backend directory to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'backend')))
+
 from app import create_app
 
 app = create_app()
