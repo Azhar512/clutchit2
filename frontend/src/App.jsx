6973af04659f2c51700dcedb52a0aa6492d3afb8
Azhar Munir <azharsiddiqui8540@gmail.com>
2025-03-13 21:30:58 +0500

    please update app.jsx and add file use-toast.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
-import Dashboard  from './pages/Dashboard';
+import Dashboard from './pages/Dashboard';
 import UploadBet from './pages/UploadBet';
 import Marketplace from './pages/Marketplace';
-import Bankroll from './pages/BankrollManagement';
 import Leaderboard from './pages/Leaderboard';
 import Profile from './pages/Profile';
 import Help from './pages/Help';
 import BankrollManagement from './pages/BankrollManagement';
+import { ToastProvider } from './components/ui/use-toast';
+
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/upload" element={<UploadBet />} />
-          <Route path="/marketplace" element={<Marketplace />} />
-          <Route path="/bankroll" element={<BankrollManagement/>} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/help" element={<Help />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <ToastProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/upload" element={<UploadBet />} />
+            <Route path="/marketplace" element={<Marketplace />} />
+            <Route path="/bankroll" element={<BankrollManagement/>} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/help" element={<Help />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ToastProvider>
   );
 }
 
