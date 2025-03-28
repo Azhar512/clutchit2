4c08ab8b45e4d945e89efd1c912ead90005452c4
Azhar Munir <azharsiddiqui8540@gmail.com>
2025-03-29 02:26:21 +0500

    update files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,103 +1,115 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider, ProtectedRoute } from './components/auth/AuthWrapper';
 import Layout from './layouts/Layout';
 import Dashboard from './pages/Dashboard';
 import UploadBet from './pages/UploadBet';
 import Marketplace from './pages/Marketplace';
 import Leaderboard from './pages/Leaderboard';
 import Profile from './pages/Profile';
-import PaymentMethodsPage from './pages/Payments';
 import Help from './pages/Help';
+import PaymentMethodsPage from './pages/Payments';
 import BankrollManagement from './pages/BankrollManagement';
 import { ToastProvider } from './components/ui/use-toast';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <Router>
+      <AuthProvider>
         <ToastProvider>
           <Routes>
-            {/* Default Route - Directly to Dashboard */}
-            <Route 
-              path="/" 
-              element={
-                
-                  <Layout>
-                    <Dashboard />
-                  </Layout>
-                
-              } 
-            />
+            {/* Public Routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+
+            {/* Default Route - Redirect to login if not authenticated */}
+            <Route path="/" element={<Navigate to="/login" />} />
+
+            {/* Protected Routes */}
             <Route 
               path="/dashboard" 
               element={
-                
+                <ProtectedRoute>
                   <Layout>
                     <Dashboard />
                   </Layout>
-                
+                </ProtectedRoute>
               } 
             />
             <Route 
               path="/upload" 
               element={
-                
+                <ProtectedRoute>
                   <Layout>
-                    <PaymentMethodsPage />
+                    <UploadBet />
                   </Layout>
-                
+                </ProtectedRoute>
               } 
             />
             <Route 
               path="/marketplace" 
               element={
-                
+                <ProtectedRoute>
                   <Layout>
                     <Marketplace />
                   </Layout>
-                
+                </ProtectedRoute>
+              } 
+            />
+             <Route 
+              path="/Payments" 
+              element={
+                <ProtectedRoute>
+                  <Layout>
+                    <PaymentMethodsPage />
+                  </Layout>
+                </ProtectedRoute>
               } 
             />
             <Route 
               path="/bankroll" 
               element={
-               
+                <ProtectedRoute>
                   <Layout>
                     <BankrollManagement />
                   </Layout>
-                
+                </ProtectedRoute>
               } 
             />
             <Route 
               path="/leaderboard" 
               element={
-                
+                <ProtectedRoute>
                   <Layout>
                     <Leaderboard />
                   </Layout>
-                
+                </ProtectedRoute>
               } 
             />
             <Route 
               path="/profile" 
               element={
-                
+                <ProtectedRoute>
                   <Layout>
                     <Profile />
                   </Layout>
-                
+                </ProtectedRoute>
               } 
             />
             <Route 
               path="/help" 
               element={
-                
+                <ProtectedRoute>
                   <Layout>
                     <Help />
                   </Layout>
-                
+                </ProtectedRoute>
               } 
             />
           </Routes>
         </ToastProvider>
+      </AuthProvider>
     </Router>
   );
 }
